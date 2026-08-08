76d5acd7c9edb50fa68c3afbbc449517c6c3350b
XNTEABDSC <2364400855@qq.com>
2026-08-09 00:01:27 +0800

    new h_select_zip implementation without a new trait

--- src/select_zip.rs
+++ src/select_zip.rs
@@ -1,34 +1,62 @@
 //! [`HSelectZippable`]
 
-use frunk::{HCons, HNil, Poly};
+use frunk::{HCons, HNil, Poly, hlist::{HMappable, HZippable, Sculptor}};
 
-use crate::type_fn::TypeFunc;
+use crate::{h_list_helpers::{HMapP, HTypeFnToMapper, HTypeMapP, HZip}, type_fn::{TypeFnAsPhantomFn, TypeFunc}};
 
-
-/// select b with [TypeFunc], and zip
-pub trait HSelectZippable<TypeFunc,B>
+/// select b with [TypeFunc], then zip
+/// We dont need a new trait to do this!
+pub fn h_select_zip<A,TypeFunc,B,BIdx>(a:A,_tf:TypeFunc,b:B)
+-> (HZip< A, HTypeMapP<A,TypeFunc>>, <B as Sculptor< HTypeMapP<A,TypeFunc>, BIdx>>::Remainder)
+where 
+	A:HMappable< Poly<HTypeFnToMapper<TypeFunc>> >,
+	B:Sculptor< HTypeMapP<A,TypeFunc>, BIdx>,
+	A:HZippable< HTypeMapP<A,TypeFunc> >
 {
-	/// the result
-	type Output;
-	/// select b with tf, and zip
-	fn select_zip(self,tf:TypeFunc,b:B)->Self::Output;
+	let (b1,b2)=b.sculpt();
+	(a.zip(b1),b2)
 }
 
-impl<TF> HSelectZippable<TF,HNil> for HNil {
-	type Output=HNil;
+// pub trait HSelectZippable<TypeFunc,B,BIdx> {
+// 	type Output;
+// 	type Remainder;
 
-	fn select_zip(self,_tf:TF,_b:HNil)->Self::Output {
-		HNil
-	}
-}
+// }
 
-impl<TF,TInputH,TInputT,BInputH,BInputT> HSelectZippable<Poly<TF>,HCons<BInputH,BInputT>> for HCons<TInputH,TInputT>
-	where TF:TypeFunc<TInputH,Output = BInputH>,
-		TInputT:HSelectZippable<Poly<TF>,BInputT>
-{
-	type Output=HCons<(TInputH,BInputH),TInputT::Output>;
+// impl<A,TypeFunc,B,BIdx,BSel> HSelectZippable<TypeFunc,B,BIdx> for A 
+// where 
+// 	A:HMappable< Poly<HTypeFnToMapper<TypeFunc>> ,Output = BSel>,
+// 	B:Sculptor< BSel, BIdx>,
+// 	A:HZippable< BSel >
+// {
+// 	type Output = HZip<A,BSel>;
+// 	type Remainder = <B as Sculptor< HTypeMapP<A,TypeFunc>, BIdx>>::Remainder;
+// }
 
-	fn select_zip(self,tf:Poly<TF>,b:HCons<BInputH,BInputT>)->Self::Output {
-		HCons{head:(self.head,b.head),tail:self.tail.select_zip(tf, b.tail)}
-	}
-}
+// /// select b with [TypeFunc], and zip
+// pub trait HSelectZippable<TypeFunc,B>
+// {
+// 	/// the result
+// 	type Output;
+// 	/// select b with tf, and zip
+// 	fn select_zip(self,tf:TypeFunc,b:B)->Self::Output;
+// }
+
+// impl<TF> HSelectZippable<TF,HNil> for HNil {
+// 	type Output=HNil;
+
+// 	fn select_zip(self,_tf:TF,_b:HNil)->Self::Output {
+// 		HNil
+// 	}
+// }
+
+// impl<TF,TInputH,TInputT,BInputH,BInputT> HSelectZippable<Poly<TF>,HCons<BInputH,BInputT>> for HCons<TInputH,TInputT>
+// 	where TF:TypeFunc<TInputH,Output = BInputH>,
+// 		TInputT:HSelectZippable<Poly<TF>,BInputT>
+// {
+// 	type Output=HCons<(TInputH,BInputH),TInputT::Output>;
+
+// 	fn select_zip(self,tf:Poly<TF>,b:HCons<BInputH,BInputT>)->Self::Output {
+// 		HCons{head:(self.head,b.head),tail:self.tail.select_zip(tf, b.tail)}
+// 	}
+// }
